d506ef6476f046a2c4d3affa644e914c8900a640
Nicksiuxs <nicksiuxs@gmail.com>
2020-08-09 10:43:30 -0500

    Se creó el seguimiento de la ruta

---
+++ TrafficLigthSimulator/Assets/Enviroment/Waypoint/WaypointNavigator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointNavigator : MonoBehaviour
+{
+    CharacterNavigationController controller;
+    public Waypoint currentWaypoint; 
+    float direction;
+
+    private void Awake() {
+        controller = GetComponent<CharacterNavigationController>();
+    }
+      void Start()
+    {
+        // controller = GetComponent<CharacterNavigationController>();
+        controller.SetDestination(currentWaypoint.GetPosition());
+        direction = Mathf.RoundToInt(UnityEngine.Random.Range(0,1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controller.reachedDestination)
+        {
+            if(direction == 0)
+            {
+                currentWaypoint = currentWaypoint.nextWaypoint;
+            }
+            else if(direction==1)
+            {
+                currentWaypoint = currentWaypoint.previousWaypoint;
+            }
+            controller.SetDestination(currentWaypoint.GetPosition());
+        }
+    }
+}
