b20f7a01fb89109e8b14bb579aef0bf68bb48453
Nicksiuxs <nicksiuxs@gmail.com>
2020-08-15 13:54:49 -0500

    Se cambió el nmbre de test y se crearon los script

---
+++ TrafficLigthSimulator/Assets/Enviroment/Reducers/Reducer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reducer : MonoBehaviour
+{
+    public GameObject trafficLight;
+    public Waypoint waypoint1;
+    // public Waypoint waypoint2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<WaypointNavigator>().currentWaypoint = waypoint1;
+        if (trafficLight.GetComponent<TrafficLightController>().isRed == true)
+        {
+            other.GetComponent<CharacterNavigationController>().setMovementSpeed(0f);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (trafficLight.GetComponent<TrafficLightController>().isRed == true)
+        {
+            other.GetComponent<CharacterNavigationController>().setMovementSpeed(0f);
+        }
+        if (trafficLight.GetComponent<TrafficLightController>().isRed == false && trafficLight.GetComponent<TrafficLightController>().isYellow == false )
+        {
+            other.GetComponent<CharacterNavigationController>().setMovementSpeed(10f);
+        }
+    }
+}
--- TrafficLigthSimulator/Assets/Enviroment/Reducers/ReducerT.cs
+++ TrafficLigthSimulator/Assets/Enviroment/Reducers/ReducerT.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Reducer : MonoBehaviour
+public class ReducerT : MonoBehaviour
 {
     public GameObject trafficLight;
+    public Waypoint waypoint1;
+    // public Waypoint waypoint2;
 
     private void OnTriggerEnter(Collider other)
     {
-
+        other.GetComponent<WaypointNavigator>().currentWaypoint = waypoint1;
         if (trafficLight.GetComponent<TrafficLightController>().isRed == true)
         {
             other.GetComponent<CharacterNavigationController>().setMovementSpeed(0f);
